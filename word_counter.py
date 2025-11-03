776356ad9d3a51f25b3be90119a40753c2cb3eec
user <user@git.com>
2025-11-03 09:43:27 +0000

    update

---
+++ word_counter.py
@@ -0,0 +1,14 @@
+import json
+
+# Load the notebook
+with open("modelAndEDA3-refined.ipynb", "r", encoding="utf-8") as f:
+    nb = json.load(f)
+
+# Extract words from Markdown cells
+word_count = 0
+for cell in nb['cells']:
+    if cell['cell_type'] == 'markdown':
+        text = "".join(cell['source'])
+        word_count += len(text.split())
+
+print("Total words (Markdown only):", word_count)
